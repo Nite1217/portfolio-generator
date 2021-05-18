a3c6b41fa83efd8930c09fab922f52a58f80d359
nikol411@hotmail.com <nikol411@hotmail.com>
2021-05-17 23:10:33 -0400

    modify html

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
 // const [name, github] = profileDataArgs;
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//  if (err) throw err;
-
-//  console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 const promptUser = () => { 
     return inquirer.prompt([
@@ -141,8 +134,14 @@ const promptProject = portfolioData => {
             });
   };
 
-promptUser()
-.then(promptProject)
-.then(portfolioData => {
-    console.log(portfolioData);
-});
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+     const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+     console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
